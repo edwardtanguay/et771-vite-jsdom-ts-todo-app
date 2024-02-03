36fbe79ab8ca05322d03a39321d1f3da119a6908
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-03 13:39:13 +0100

    replacing correctly

--- src/components/Todo.ts
+++ src/components/Todo.ts
@@ -11,21 +11,23 @@ export const Todo = (todo: ITodo, todos: ITodo[]) => {
 		if (todoElem && todoLabelElem) {
 			todoLabelElem.addEventListener('click', () => {
 				todo.finished = !todo.finished;
-				console.log(todos);
+				todoElem.innerHTML = Todo(todo, todos);
 			});
 		}
 	}, 0);
 
 	return /*html*/ `
-<div class="bg-gray-800 mb-2 p-3 rounded border border-gray-700 flex justify-between" id="${htmlTodoId}">
-	<section class="flex gap-2 align-middle">
-		<p><input type="checkbox" id="${htmlCheckboxId}" ${todo.finished ? ' checked' : ''}/></p>
-		<label class="select-none cursor-pointer hover:text-gray-50 ${todo.finished ? `line-through` : ''}" for="${htmlCheckboxId}">${todo.text}</label>
-	</section>
-	<section class="flex gap-2">
-		<button><i class="fa fa-pencil text-green-500 hover:text-green-300" aria-hidden="true"></i></button>
-		<button><i class="fa fa-trash text-red-500 hover:text-red-300" aria-hidden="true"></i></button>
-	</section>
+<div id="${htmlTodoId}">
+	<div class="bg-gray-800 mb-2 p-3 rounded border border-gray-700 flex justify-between">
+		<section class="flex gap-2 align-middle">
+			<p><input type="checkbox" id="${htmlCheckboxId}" ${todo.finished ? ' checked' : ''}/></p>
+			<label class="select-none cursor-pointer hover:text-gray-50 ${todo.finished ? `line-through` : ''}" for="${htmlCheckboxId}">${todo.text}</label>
+		</section>
+		<section class="flex gap-2">
+			<button><i class="fa fa-pencil text-green-500 hover:text-green-300" aria-hidden="true"></i></button>
+			<button><i class="fa fa-trash text-red-500 hover:text-red-300" aria-hidden="true"></i></button>
+		</section>
+	</div>
 </div>
 	`;
 }
