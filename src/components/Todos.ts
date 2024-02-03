137642e713dc3d5514809d38bb97c3a5c0b04752
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-03 14:01:27 +0100

    debuggingArea

--- src/components/Todos.ts
+++ src/components/Todos.ts
@@ -1,26 +1,35 @@
-import { todos } from '../data.js';
-import { ITodo } from '../interfaces.js';
-import { Todo } from './Todo.js';
+import { todos } from "../data.js";
+import { ITodo } from "../interfaces.js";
+import { Todo } from "./Todo.js";
+import * as config from "../config.js";
 
 export const Todos = () => {
+	setTimeout(() => {
+		render(todos);
+	}, 0);
+
 	return /*html*/ `
 <div class="mt-4">
-	<div id="todos-component">
-		${todos.map(todo => {
-			return Todo(todo, todos);
-		}).join('')}
-	</div>
+	<div id="todos-component"></div>
 </div>
-`
-}
+`;
+};
 
 export const render = (todos: ITodo[]) => {
-	const todosComponentElem = document.querySelector<HTMLDivElement>('#todos-component');
-	let html = '';
+	const todosComponentElem =
+		document.querySelector<HTMLDivElement>("#todos-component");
+	let html = "";
 	if (todosComponentElem) {
 		for (const todo of todos) {
 			html += Todo(todo, todos);
 		}
+		if (config.debuggingIsOn()) {
+			html += `
+			<div class="debuggingArea">
+			<p>debugging info</p>
+			</div>
+			`;
+		}
 		todosComponentElem.innerHTML = html;
 	}
-}
+};
