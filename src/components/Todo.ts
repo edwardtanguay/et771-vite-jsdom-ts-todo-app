109151b3013b8c661625a9d1fe37df6f4485371c
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-03 13:05:22 +0100

    hover ok

--- src/components/Todo.ts
+++ src/components/Todo.ts
@@ -1,11 +1,12 @@
 import { ITodo } from "../interfaces"
 
 export const Todo = (todo: ITodo) => {
+	const htmlId = `checkbox-${todo.suuid}`;
 	return /*html*/ `
 <div class="bg-gray-800 mb-2 p-3 rounded border border-gray-700 flex justify-between">
 	<section class="flex gap-2 align-middle">
-		<p><input type="checkbox"/></p>
-		<p>${todo.text}</p>
+		<p><input type="checkbox" id="${htmlId}" ${todo.finished ? ' checked' : ''}/></p>
+		<label class="select-none cursor-pointer hover:text-gray-50 ${todo.finished ? `line-through` : ''}" for="${htmlId}">${todo.text}</label>
 	</section>
 	<section class="flex gap-2">
 		<button><i class="fa fa-pencil text-green-500 hover:text-green-300" aria-hidden="true"></i></button>
