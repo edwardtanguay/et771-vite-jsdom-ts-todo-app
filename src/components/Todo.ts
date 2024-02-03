8ae408e82516aa31579e5edb2b95aa102e44ad81
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-03 13:18:42 +0100

    timeout style works

--- src/components/Todo.ts
+++ src/components/Todo.ts
@@ -1,12 +1,23 @@
 import { ITodo } from "../interfaces"
 
+
 export const Todo = (todo: ITodo) => {
-	const htmlId = `checkbox-${todo.suuid}`;
+	const htmlSectionId = `section-${todo.suuid}`;
+	const htmlCheckboxId = `checkbox-${todo.suuid}`;
+
+	setTimeout(() => {
+		const todoElem = document.querySelector<HTMLDivElement>(`#${htmlSectionId}`);
+		if (todoElem) {
+			todoElem.style.display = 'none';
+		}
+		console.log(111, todo.suuid);
+	}, 0);
+
 	return /*html*/ `
 <div class="bg-gray-800 mb-2 p-3 rounded border border-gray-700 flex justify-between">
-	<section class="flex gap-2 align-middle">
-		<p><input type="checkbox" id="${htmlId}" ${todo.finished ? ' checked' : ''}/></p>
-		<label class="select-none cursor-pointer hover:text-gray-50 ${todo.finished ? `line-through` : ''}" for="${htmlId}">${todo.text}</label>
+	<section class="flex gap-2 align-middle" id="${htmlSectionId}">
+		<p><input type="checkbox" id="${htmlCheckboxId}" ${todo.finished ? ' checked' : ''}/></p>
+		<label class="select-none cursor-pointer hover:text-gray-50 ${todo.finished ? `line-through` : ''}" for="${htmlCheckboxId}">${todo.text}</label>
 	</section>
 	<section class="flex gap-2">
 		<button><i class="fa fa-pencil text-green-500 hover:text-green-300" aria-hidden="true"></i></button>
