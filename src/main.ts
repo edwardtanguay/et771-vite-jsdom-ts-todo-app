cdbae9db18c00f82bc9d982793e8d33b0819bde2
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-04 15:21:43 +0100

    half about

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { getCurrentPage, getMenu } from './Router';
-import './style.css';
+import './style.scss';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
 <main class="bg-slate-700 p-4 md:rounded-lg w-full md:w-[20rem] md:min-h-[40rem]">
--- src/pages/PageAbout.ts
+++ src/pages/PageAbout.ts
@@ -1,5 +1,36 @@
 export const PageAbout = () => {
 	return /*html*/ `
-	<p>page about</p>
+<section class="pageAbout">
+	<h2>purpose of this site</h2>
+	<ul>
+		<li>to learn how to make <em>interactive applications</em> with plain JavaScript</li>
+	</ul>
+	<h2>features</h2>
+	<ul>
+		<li>the site is built with <em>Vite JS-DOM (Vanilla)</em> which provides a modern JavaScript npm development environment</li>
+		<li>simple <em>state management</em> gives you the ability to change variables and then rerender the entire site, similar to React, Vue.js, Angular, etc.</li>
+		<li>the entire site is written in <em>TypeScript</em> which gives you type safety, intellisense, code-completion, protection from publishing bugs, etc.</li>
+	</ul>
+	<h2>nnn</h2>
+	<ul>
+		<li>nnn</li>
+	</ul>
+	<h2>nnn</h2>
+	<ul>
+		<li>nnn</li>
+	</ul>
+	<h2>nnn</h2>
+	<ul>
+		<li>nnn</li>
+	</ul>
+	<h2>nnn</h2>
+	<ul>
+		<li>nnn</li>
+	</ul>
+	<h2>nnn</h2>
+	<ul>
+		<li>nnn</li>
+	</ul>
+</section>
 	`
 }
