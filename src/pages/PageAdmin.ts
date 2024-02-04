14438a5fc2d7939e3bf7fdbb7cc2374e06eb39fe
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-05 00:04:33 +0100

    showing debugging info

--- src/pages/PageAdmin.ts
+++ src/pages/PageAdmin.ts
@@ -1,5 +1,20 @@
 export const PageAdmin = () => {
+
+	setTimeout(() => {
+		const checkboxDebuggingElem = document.querySelector<HTMLInputElement>('#checkboxDebugging');
+
+		if (checkboxDebuggingElem) {
+			checkboxDebuggingElem.addEventListener('click', () => {
+
+			});
+		}
+	}, 0);
+
 	return /*html*/ `
-	<p>page admin</p>
+	<div class="text-slate-300">
+		<form>
+			<input id="checkboxDebugging" type="checkbox"/> <label for="checkboxDebugging" class="select-none cursor-pointer hover:text-slate-50">show debugging information</label> 
+		</form>
+	</div>
 	`
 }
