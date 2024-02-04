f2af4d14205c1f9f18f60ab75b7d8a2ff06916b8
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-04 11:58:31 +0100

    centered styled

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { getCurrentPage, getMenu } from './Router';
 import './style.css';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
-<h1 class="text-2xl text-yellow-300">Todo App</h1>
-${getMenu()}
-${getCurrentPage()}
+<main class="bg-slate-700 p-4 rounded-lg w-[20rem] min-h-[30rem]">
+	<h1 class="text-2xl text-yellow-300">Todo App</h1>
+	${getMenu()}
+	${getCurrentPage()}
+</main>
 `
 
